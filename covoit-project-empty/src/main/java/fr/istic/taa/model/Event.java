374c5b4e98b72b029f0d203b80c09eb81ee21d49
mohssine.amhachi <mohssine.amhachi@gmail.com>
2014-10-19 14:50:55 +0200

    Add : Event and participation

---
+++ covoit-project-empty/src/main/java/fr/istic/taa/model/Event.java
@@ -0,0 +1,436 @@
+package fr.istic.taa.model;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Date;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+ 
+@javax.persistence.Entity 
+public class Event
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	 
+	@javax.persistence.Column(nullable = false) 
+	protected long idEvent;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	 
+	@javax.persistence.Column(nullable = false) 
+	protected String adresse;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	 
+	@javax.persistence.Column(nullable = false) 
+	protected String name;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	 
+	@javax.persistence.Temporal(javax.persistence.TemporalType.DATE) 
+	@javax.persistence.Column(nullable = false) 
+	protected Date date;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	 
+	@javax.persistence.OneToMany(mappedBy = "event") 
+	protected Set<User> createur;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	 
+	@javax.persistence.OneToMany(mappedBy = "event") 
+	protected Set<Comment> comment;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	 
+	@javax.persistence.ManyToOne 
+	@javax.persistence.JoinColumn(nullable = false) 
+	protected Participation participation;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	@javax.persistence.Id 
+	@javax.persistence.Column(nullable = false) 
+	protected final Long id = 0L;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Event(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void basicSetParticipation(Participation myParticipation) {
+		if (this.participation != myParticipation) {
+			if (myParticipation != null){
+				if (this.participation != myParticipation) {
+					Participation oldparticipation = this.participation;
+					this.participation = myParticipation;
+					if (oldparticipation != null)
+						oldparticipation.removeEvent(this);
+				}
+			}
+		}	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public long getIdEvent() {
+		return this.idEvent;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String getAdresse() {
+		return this.adresse;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String getName() {
+		return this.name;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public Date getDate() {
+		return this.date;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public Set<User> getCreateur() {
+		if(this.createur == null) {
+				this.createur = new HashSet<User>();
+		}
+		return (Set<User>) this.createur;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public Set<Comment> getComment() {
+		if(this.comment == null) {
+				this.comment = new HashSet<Comment>();
+		}
+		return (Set<Comment>) this.comment;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public Participation getParticipation() {
+		return this.participation;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public long getId() {
+		return this.id;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void addAllCreateur(Set<User> newCreateur) {
+		if (this.createur == null) {
+			this.createur = new HashSet<User>();
+		}
+		for (User tmp : newCreateur)
+			tmp.setEvent(this);
+			
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void addAllComment(Set<Comment> newComment) {
+		if (this.comment == null) {
+			this.comment = new HashSet<Comment>();
+		}
+		for (Comment tmp : newComment)
+			tmp.setEvent(this);
+			
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void removeAllCreateur(Set<User> newCreateur) {
+		if(this.createur == null) {
+			return;
+		}
+		
+		this.createur.removeAll(newCreateur);	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void removeAllComment(Set<Comment> newComment) {
+		if(this.comment == null) {
+			return;
+		}
+		
+		this.comment.removeAll(newComment);	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setIdEvent(long myIdEvent) {
+		this.idEvent = myIdEvent;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setAdresse(String myAdresse) {
+		this.adresse = myAdresse;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setName(String myName) {
+		this.name = myName;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setDate(Date myDate) {
+		this.date = myDate;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void addCreateur(User newCreateur) {
+		if(this.createur == null) {
+			this.createur = new HashSet<User>();
+		}
+		
+		if (this.createur.add(newCreateur))
+			newCreateur.basicSetEvent(this);	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void addComment(Comment newComment) {
+		if(this.comment == null) {
+			this.comment = new HashSet<Comment>();
+		}
+		
+		if (this.comment.add(newComment))
+			newComment.basicSetEvent(this);	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setParticipation(Participation myParticipation) {
+		this.basicSetParticipation(myParticipation);
+		myParticipation.addEvent(this);	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetIdEvent() {
+		this.idEvent = 0L;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetAdresse() {
+		this.adresse = "";	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetName() {
+		this.name = "";	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetDate() {
+		this.date = new Date();	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void removeCreateur(User oldCreateur) {
+		if(this.createur == null)
+			return;
+		
+		if (this.createur.remove(oldCreateur))
+			oldCreateur.unsetEvent();
+			
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void removeComment(Comment oldComment) {
+		if(this.comment == null)
+			return;
+		
+		if (this.comment.remove(oldComment))
+			oldComment.unsetEvent();
+			
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetParticipation() {
+		if (this.participation == null)
+			return;
+		Participation oldparticipation = this.participation;
+		this.participation = null;
+		oldparticipation.removeEvent(this);	
+	}
+	
+}
+
