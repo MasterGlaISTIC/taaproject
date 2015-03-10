b5c11a8a0e677b0b8d71b2831f13d86285c0d082
MrChaabi <chaabi@CHAABI-Master2GL>
2015-03-10 23:17:52 +0100

    add: project angularJS

---
+++ angularjs/src/main/webapp/script.js
@@ -0,0 +1,99 @@
+//create the module and name it scotchApp
+var scotchApp = angular.module('scotchApp', ['ngRoute','ngResource']);
+
+//configure our routes
+scotchApp.config(function($routeProvider) {
+	$routeProvider
+	// route for the home page
+	
+
+	// route for the about page
+	.when('/about', {
+		templateUrl : 'pages/about.html',
+		controller  : 'aboutController'
+	})
+
+	// route for the contact page
+	.when('/contact', {
+		templateUrl : 'pages/contact.html',
+		controller  : 'contactController'
+	})
+	
+	.when('/', {
+		templateUrl : 'pages/home.html',
+		controller  : 'mainController'
+	})
+
+});
+
+//create the controller and inject Angular's $scope
+scotchApp.controller('mainController', function($scope , $http, $location) {	
+	 $scope.authen = function () {
+		 $http.post('http://localhost:8080/covoit-service/rest/users/auth', {email:$scope.user.email, passwd:$scope.user.passwd})
+		 .success(function(data, status, headers, config) {
+			 if(data == "true"){
+				  $location.path('/about')
+			  } else {
+				  $scope.msgErreur="Email ou mot de passe est incorrecte"
+			  }
+		  }).
+		  error(function(data, status, headers, config) {
+			  alert("erreur "+status)
+		  });
+     }
+	
+	
+	
+//	this.reset
+	
+	
+
+//	 $http.post('http://localhost:8080/covoit-service/rest/users/auth', {email:'mohssine.amhachi@gmail.com', passwd:'123'})
+//	 .success(function(data, status, headers, config) {
+//		  alert("autehnticate")
+//	  }).
+//	  error(function(data, status, headers, config) {
+//		  alert("erreur ")
+//	  });
+	// exmple 
+	
+	// Simple POST request example (passing data) :
+//	$http.post('http://localhost:8080/covoit-service/rest/users/addUser', {email:'hellaaa'}).
+//	  success(function(data, status, headers, config) {
+//		  alert("success")
+//	  }).
+//	  error(function(data, status, headers, config) {
+//		  alert("erreur ")
+//	  });
+	
+//	$http.get("http://localhost:8080/covoit-service/rest/users/users").then(function(data ) {
+//		$scope.auth = data.data;
+//		alert(data.data);
+//	})
+});
+
+scotchApp.controller('aboutController', function($scope, $http, $location) {
+	alert("s")
+	$http.get("http://localhost:8080/covoit-service/rest/users/users").then(function(data) {
+		$scope.auth = data.data;
+	})
+});
+
+scotchApp.controller('contactController', function($scope) {
+	$http.get("http://localhost:8080/covoit-service/rest/cars/cars").then(function(data) {
+		$scope.car = data.data;
+	})
+	});
+
+scotchApp.controller('addEventsController', function($scope) {
+//	$http.post("http://localhost:8080/covoit-service/rest/cars/cars",
+//			  encodeURI("date="+date) + "&" + encodeURI("place="+place) + "&" + encodeURI("desc="+desc),
+//			  {headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/x-www-form-urlencoded'}}).success(callback);
+});
+
+
+
+
+
+
+
