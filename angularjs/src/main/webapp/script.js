522187e77e5e52bfa269c8634abf6efafb910821
MrChaabi <chaabi@CHAABI-Master2GL>
2015-03-12 17:52:22 +0100

    Signed-off-by: MrChaabi <chaabi@CHAABI-Master2GL>

--- angularjs/src/main/webapp/script.js
+++ angularjs/src/main/webapp/script.js
@@ -5,19 +5,30 @@ var scotchApp = angular.module('scotchApp', ['ngRoute','ngResource']);
 scotchApp.config(function($routeProvider) {
 	$routeProvider
 	// route for the home page
-	
+
 
 	// route for the about page
-	.when('/about', {
-		templateUrl : 'pages/about.html',
-		controller  : 'aboutController'
+	.when('/addevent', {
+		templateUrl : 'pages/addEvent.html',
+		controller  : 'addEventController'
 	})
 
 	// route for the contact page
-	.when('/contact', {
-		templateUrl : 'pages/contact.html',
+	.when('/events', {
+		templateUrl : 'pages/events.html',
 		controller  : 'contactController'
 	})
+
+	.when('/inscription', {
+		templateUrl : 'pages/inscription.html',
+		controller  : 'inscriptionController'
+	})
+
+	.when('/addEvent', {
+		templateUrl : 'pages/addEvent.html',
+		controller  : 'addEventController'
+	})
+	
 	
 	.when('/', {
 		templateUrl : 'pages/home.html',
@@ -28,48 +39,20 @@ scotchApp.config(function($routeProvider) {
 
 //create the controller and inject Angular's $scope
 scotchApp.controller('mainController', function($scope , $http, $location) {	
-	 $scope.authen = function () {
-		 $http.post('http://localhost:8080/covoit-service/rest/users/auth', {email:$scope.user.email, passwd:$scope.user.passwd})
-		 .success(function(data, status, headers, config) {
-			 if(data == "true"){
-				  $location.path('/about')
-			  } else {
-				  $scope.msgErreur="Email ou mot de passe est incorrecte"
-			  }
-		  }).
-		  error(function(data, status, headers, config) {
-			  alert("erreur "+status)
-		  });
-     }
-	
-	
-	
-//	this.reset
-	
-	
+	$scope.authen = function () {
+		$http.post('http://localhost:8080/covoit-service/rest/users/auth', {email:$scope.user.email, passwd:$scope.user.passwd})
+		.success(function(data, status, headers, config) {
+			if(data == "true"){
+				$location.path('/events')
+			} else {
+				$scope.msgErreur="Email ou mot de passe est incorrecte"
+			}
+		}).
+		error(function(data, status, headers, config) {
+			alert("erreur "+status)
+		});
+	}
 
-//	 $http.post('http://localhost:8080/covoit-service/rest/users/auth', {email:'mohssine.amhachi@gmail.com', passwd:'123'})
-//	 .success(function(data, status, headers, config) {
-//		  alert("autehnticate")
-//	  }).
-//	  error(function(data, status, headers, config) {
-//		  alert("erreur ")
-//	  });
-	// exmple 
-	
-	// Simple POST request example (passing data) :
-//	$http.post('http://localhost:8080/covoit-service/rest/users/addUser', {email:'hellaaa'}).
-//	  success(function(data, status, headers, config) {
-//		  alert("success")
-//	  }).
-//	  error(function(data, status, headers, config) {
-//		  alert("erreur ")
-//	  });
-	
-//	$http.get("http://localhost:8080/covoit-service/rest/users/users").then(function(data ) {
-//		$scope.auth = data.data;
-//		alert(data.data);
-//	})
 });
 
 scotchApp.controller('aboutController', function($scope, $http, $location) {
@@ -80,35 +63,49 @@ scotchApp.controller('aboutController', function($scope, $http, $location) {
 });
 
 scotchApp.controller('contactController', function($scope, $http, $location) {
-//	 $scope.authen = function () {
-//		 $http.post('http://localhost:8080/covoit-service/rest/users/auth', {email:$scope.user.email, passwd:$scope.user.passwd})
-//		 .success(function(data, status, headers, config) {
-//			 if(data == "true"){
-//				  $location.path('/about')
-//			  } else {
-//				  $scope.msgErreur="Email ou mot de passe est incorrecte"
-//			  }
-//		  }).
-//		  error(function(data, status, headers, config) {
-//			  alert("erreur "+status)
-//		  });
-//     }
-	// add for test
-	$http.get("http://localhost:8080/covoit-service/rest/users/users").then(function(data) {
-		$scope.auth = data.data;
+	$http.get("http://localhost:8080/covoit-service/rest/journey/journeys").then(function(data) {
+		$scope.event = data.data;
 	})
-	});
+});
 
 scotchApp.controller('addEventsController', function($scope) {
 	// add for test
 	$http.get("http://localhost:8080/covoit-service/rest/users/users").then(function(data) {
 		$scope.auth = data.data;
 	})
+});
+
+scotchApp.controller('inscriptionController', function($scope, $http, $location) {
+	$scope.inscription = function () {
+		$http.post('http://localhost:8080/covoit-service/rest/users/addUser', {email:$scope.user.email, passwd:$scope.user.passwd, name:$scope.user.name, location:$scope.user.location})
+		.success(function(data, status, headers, config) {
+
+			alert("success")
+			$location.path('/')
+
+		}).
+		error(function(data, status, headers, config) {
+			alert("erreur "+status)
+		});
+
+	}
+});
+
+
+	scotchApp.controller('addEventController', function($scope, $http, $location) {
+		$scope.addEvent = function () {
+			$http.post('http://localhost:8080/covoit-service/rest/journey/addJourney', {name:$scope.user.name, adresse:$scope.user.adresse, date:$scope.user.date})
+			.success(function(data, status, headers, config) {
+
+				$location.path('/events')
+
+			}).
+			error(function(data, status, headers, config) {
+				alert("erreur "+status)
+			});
+
+		}
 	});
-
-
-
-
 
 
 
